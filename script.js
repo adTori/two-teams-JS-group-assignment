953a645f782ae911655dc489a58fee5b69a03d34
ablueone <vls_89@hotmail.com>
2025-11-24 09:10:53 +0100

    organized and commented the js code, and made the change team button work

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // CHANGE A TEAM NAME
-
 let newATeam = document.querySelector(".aNamn");
 
 const bytA = document.querySelector("#bytA");
@@ -8,11 +7,12 @@ const btnA = document.querySelector(".btnA");
 
 btnA.addEventListener("click", () => {
     newATeam.innerText = bytA.value
+
+    document.querySelector("#bytA").value = "";
 })
 
 
 // CHANGE B TEAM NAME
-
 let newBTeam = document.querySelector(".bNamn");
 
 const bytB = document.querySelector("#bytB");
@@ -21,44 +21,26 @@ const btnB = document.querySelector(".btnB");
 
 btnB.addEventListener("click", () => {
     newBTeam.innerText = bytB.value
+
+    document.querySelector("#bytB").value = "";
 })
 
 
-// ADD PLAYER
-
+// CREATE INPUT FOR PLAYERS AND AN ADD BUTTON
 let newPlayer = document.querySelector("#playerAdd");
 let btnAdd = document.querySelector(".btnAdd");
 
+// CREATE UL (UNORDERED LISTS) FOR EACH TEAM
 const ulA = document.querySelector(".aSpelare");
 const ulB = document.querySelector(".bSpelare");
 
-// let teamA = [];
-// let teamB = [];
-
-// function addPlayer(){
-
-// }
-
-
-
+// CREATE LI (LIST ITEMS) FOR THE PLAYERS WHEN ADD BUTTON IS CLICKED
 btnAdd.addEventListener("click", () => {
     let li = document.createElement('li')
     
-    const leave = document.createElement("button")
-    leave.innerText = "Leave Team"
-    leave.style.backgroundColor = "yellow"
-
-    leave.addEventListener("click", () => {
-        li.remove();
-    })
-
-    const change = document.createElement("button")
-    change.innerText = "Change Team"
-    change.style.backgroundColor = "chartreuse"
-
-    li.innerText = newPlayer.value
-    li.append(leave, change)
-
+// ADD TEAMPLAYERS (MAXIMUM 10)
+// IF MORE THEN 5 IN FIRST TEAM, NEW PLAYERS ARE PUT IN THE SECOND TEAM
+// ALERT IF MORE THEN 10 PLAYERS IN TOTAL
     if (ulA.children.length < 5) {
         ulA.append(li)
     } else if (ulB.children.length < 5) {
@@ -67,23 +49,43 @@ btnAdd.addEventListener("click", () => {
         alert("You've maxed out both teams!")
     }
 
-    // change.addEventListener("click", () => {
-    // if (ulA.children.length <= 5 || ulB.children.length <= 5) {
-    //     append.event.target.closest(".lag")
-    // }})
+// CREATE A YELLOW LEAVE BUTTON
+const leave = document.createElement("button")
+    leave.innerText = "Leave Team"
+    leave.style.backgroundColor = "yellow"
 
+// CREATE A GREEN-ISH CHANGE TEAM BUTTON
+const change = document.createElement("button")
+    change.innerText = "Change Team"
+    change.style.backgroundColor = "chartreuse"
+
+// WHEN INPUT OF NEW PLAYER IS ADDED TO THE LI, THE LEAVE BUTTON + CHANGE TEAM BUTTON WILL ALSO BE ADDED
+    li.innerText = newPlayer.value
+    li.append(leave, change)
+
+// LEAVE TEAM WHEN LEAVE BUTTON IS CLICKED
+    leave.addEventListener("click", () => {
+        li.remove();
+    })
+
+// CHANGE TEAM WHEN CHANGE TEAM BUTTON IS CLICKED
     change.addEventListener("click", () => {
-        if (ulA.children.length <= 5) {
-            ulB.append(li)
-        } else if (ulB.children.length <= 5) {
-            ulA.append(li) 
+        let currentTeam = li.parentElement;
+
+        if (currentTeam.id === "ulA") {
+            if (ulB.children.length < 5) {
+                ulB.append(li);
+            } else {
+                alert("The other team is full!");
+            }
         } else {
-            alert("The other team is full!")
-        }})
+            if (ulA.children.length < 5) {
+                ulA.append(li);
+            } else {
+                alert("The other team is full!");
+            }
+        }
+    })
 
     document.querySelector("#playerAdd").value = "";
 });
-
-
-
-// LEAVE TEAM
